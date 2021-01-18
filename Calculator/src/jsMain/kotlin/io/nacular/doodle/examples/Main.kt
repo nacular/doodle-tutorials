6ccde0785edb52759116e918e4df8f32ef91c031
neddy <nickeddy@gmail.com>
2021-01-17 22:33:07 -0800

    - Updating font handling for Doodle 0.4.0

--- Calculator/src/jsMain/kotlin/io/nacular/doodle/examples/Main.kt
+++ Calculator/src/jsMain/kotlin/io/nacular/doodle/examples/Main.kt
@@ -1,25 +1,15 @@
 package io.nacular.doodle.examples
 
+import io.nacular.doodle.application.Modules.Companion.FontModule
 import io.nacular.doodle.application.Modules.Companion.PointerModule
 import io.nacular.doodle.application.application
-import io.nacular.doodle.drawing.FontDetector
-import io.nacular.doodle.drawing.impl.FontDetectorImpl
-import org.kodein.di.Kodein.Module
-import org.kodein.di.erased.bind
 import org.kodein.di.erased.instance
-import org.kodein.di.erased.singleton
 
 /**
  * Creates a [CalculatorApp]
  */
 fun main() {
-    application(modules = listOf(
-        PointerModule,
-        Module(name = "AppModule") {
-            // Used to get fonts that should've been loaded
-            bind<FontDetector>() with singleton { FontDetectorImpl(instance(), instance(), instance()) }
-        }
-    )) {
+    application(modules = listOf(FontModule, PointerModule)) {
         // load app
         CalculatorApp(instance(), instance(), instance(), NumberFormatterImpl())
     }
