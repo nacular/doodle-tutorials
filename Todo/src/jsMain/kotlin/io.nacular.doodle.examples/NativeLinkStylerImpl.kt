e7dfd043d4d5b919fc3fb2753285f90ce0443bfb
neddy <nickeddy@gmail.com>
2021-02-05 09:07:00 -0800

    - Updating Todo app

---
+++ Todo/src/jsMain/kotlin/io.nacular.doodle.examples/NativeLinkStylerImpl.kt
@@ -0,0 +1,9 @@
+package io.nacular.doodle.examples
+
+import io.nacular.doodle.controls.buttons.HyperLink
+import io.nacular.doodle.core.Behavior
+import io.nacular.doodle.theme.native.NativeHyperLinkBehaviorBuilder
+
+class NativeLinkStylerImpl(private val delegate: NativeHyperLinkBehaviorBuilder): NativeLinkStyler {
+    override fun invoke(link: HyperLink, behavior: Behavior<HyperLink>) = delegate(link, behavior)
+}
