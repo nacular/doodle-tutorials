fd17b79d22989735c54ad4007092c2c578b9cc2f
neddy <nickeddy@gmail.com>
2021-09-22 07:30:15 -0700

    - Fixed number desktop formatting

--- Calculator/src/jvmMain/kotlin/io/nacular/doodle/examples/NumberFormatter.kt
+++ Calculator/src/jvmMain/kotlin/io/nacular/doodle/examples/NumberFormatter.kt
@@ -1,5 +1,8 @@
 package io.nacular.doodle.examples
 
+import java.text.NumberFormat
+import java.util.Locale
+
 /**
  * Created by Nicholas Eddy on 8/19/20.
  */
@@ -14,8 +17,10 @@ actual interface NumberFormatter {
  * Simple impl since we are only using jvm for testing.
  */
 class NumberFormatterImpl: NumberFormatter {
-    override fun invoke(number: Int   ) = number.toString()
-    override fun invoke(number: Long  ) = number.toString()
-    override fun invoke(number: Float ) = number.toString()
-    override fun invoke(number: Double) = number.toString()
+    private val formatter = NumberFormat.getInstance(Locale.ENGLISH)
+
+    override fun invoke(number: Int   ): String = formatter.format(number)
+    override fun invoke(number: Long  ): String = formatter.format(number)
+    override fun invoke(number: Float ): String = formatter.format(number)
+    override fun invoke(number: Double): String = formatter.format(number)
 }
