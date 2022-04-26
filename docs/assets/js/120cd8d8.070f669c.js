c77718c4c41715772e14927bd00714c914ada6f4
neddy <nickeddy@gmail.com>
2022-04-25 19:28:08 -0700

    - Updated docusaurus - Updated problematic dependencies

--- docs/assets/js/120cd8d8.070f669c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[438],{7144:function(e){e.exports={blogPosts:[]}}}]);
--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -28,6 +28,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        googleAnalytics: {
+          trackingID: 'G-KN5YH7BJYG',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
