604b02ac9890e55ae884e23423c4bc136d92c688
neddy <nickeddy@gmail.com>
2022-04-27 19:20:24 -0700

    - docs

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -28,7 +28,7 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-        googleAnalytics: {
+        gtag: {
           trackingID: 'G-KN5YH7BJYG',
           anonymizeIP: true,
         },
