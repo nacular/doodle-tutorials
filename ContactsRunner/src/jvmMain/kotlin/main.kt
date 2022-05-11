246572d87e6b21539623c27744a44db20a5ff253
neddy <nickeddy@gmail.com>
2022-05-11 07:31:40 -0700

    - [Fix] Malformed svg image data - Doodle -> 0.7.2

--- ContactsRunner/src/jvmMain/kotlin/main.kt
+++ ContactsRunner/src/jvmMain/kotlin/main.kt
@@ -55,15 +55,7 @@ fun main() {
         // load app
         ContactsApp(
             theme             = instance(),
-            assets            = { val a = AppConfigImpl(instance(), instance()); object: AppConfig by a {
-                // FIXME: Remove when https://github.com/JetBrains/skiko/issues/518 (which causes app crash) is fixed
-                val exclamation = "M3.402 0C4.752 0 5.454.675 5.427 1.998 5.4 4.05 5.4 5.4 4.725 10.125 4.59 11.07 4.32 11.475 3.375 11.475 3.375 11.475 2.025 11.475 2.025 11.475 1.08 11.475.81 11.07.675 10.125 0 5.535 0 4.05 0 2.052 0 .675.675 0 2.025 0M2.7 13.5C3.429 13.5 4.104 13.797 4.617 14.283 5.103 14.796 5.4 15.471 5.4 16.2 5.4 17.685 4.185 18.9 2.7 18.9 1.215 18.9 0 17.685 0 16.2 0 15.471.297 14.796.783 14.283 1.296 13.797 1.971 13.5 2.7 13.5 2.7 13.5 2.7 13.5 2.7 13.5"
-
-                override val editIcon   = exclamation
-                override val nameIcon   = exclamation
-                override val phoneIcon  = exclamation
-                override val searchIcon = exclamation
-            } },
+            assets            = { AppConfigImpl(instance(), instance()) },
             router            = instance(),
             Header            = factory(),
             display           = instance(),
